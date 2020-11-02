21e03af7151fd7925b436788c5723e235ca39811
Hongjun <nanjingchj@gmail.com>
2020-11-02 09:18:20 -0500

    improved wheel traction | sprinting

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
@@ -11,6 +12,11 @@ public class CameraController : MonoBehaviour
 {
     private Camera m_Camera;
     private bool m_MouseLocked;
+    private bool m_Sprinting;
+    private float m_Fov = 60.0f;
+    private float m_FovTarget = 70.0f;
+    private DateTime m_FovAnimationStartTime;
+    private bool m_DoFovAnimation;
 
     void Start()
     {
@@ -39,16 +45,87 @@ public class CameraController : MonoBehaviour
         {
             m_Camera.transform.Rotate(Vector3.up, 300.0f * Time.deltaTime * Input.GetAxis("Mouse X"));
             m_Camera.transform.Rotate(Vector3.right, -300.0f * Time.deltaTime * Input.GetAxis("Mouse Y"));
-            m_Camera.transform.Translate(50.0f * Time.deltaTime * GetUserInput()); // relative
+            Vector4 input = GetUserInput();
+            
+            if (Math.Abs(input.w - 1.0f) < 0.01f)
+            {
+                if (!m_Sprinting)
+                {
+                    m_Sprinting = true;
+                    // start fov animation
+                    m_FovAnimationStartTime = DateTime.Now;
+                    m_DoFovAnimation = true;
+                }
+
+                if (m_DoFovAnimation)
+                {
+                    TimeSpan deltaTime = DateTime.Now - m_FovAnimationStartTime;
+                    if (deltaTime.Milliseconds < 75)
+                    {
+                        // do animation
+                        m_Camera.fieldOfView = Mathf.Lerp(m_Fov, m_FovTarget, deltaTime.Milliseconds / 100.0f);
+                    }
+                    else
+                    {
+                        m_DoFovAnimation = false;
+                    }
+                }
+                else
+                {
+                    m_Camera.fieldOfView = m_FovTarget;
+                }
+
+            }
+            else
+            {
+                if (m_Sprinting)
+                {
+                    m_Sprinting = false;
+                    m_Camera.fieldOfView = m_Fov;
+                    // start fov animation
+                    m_FovAnimationStartTime = DateTime.Now;
+                    m_DoFovAnimation = true;
+                }
+
+                if (m_DoFovAnimation)
+                {
+                    TimeSpan deltaTime = DateTime.Now - m_FovAnimationStartTime;
+                    if (deltaTime.Milliseconds < 75)
+                    {
+                        // do animation
+                        m_Camera.fieldOfView = Mathf.Lerp(m_FovTarget, m_Fov, deltaTime.Milliseconds / 100.0f);
+                    }
+                    else
+                    {
+                        m_DoFovAnimation = false;
+                    }
+                }
+                else
+                {
+                    m_Camera.fieldOfView = m_Fov;
+                }
+            }
+
+            input *= 25.0f * Time.deltaTime;
+            if (m_Sprinting)
+            {
+                input *= 2.0f;
+            }
+
+            // up / down motion is absolute
+            m_Camera.transform.Translate(new Vector3(0.0f, input.y, 0.0f), Space.World);
+            // forward / backward / left / right motion is relative to the plane parallel to the ground through the camera
+            m_Camera.transform.Translate(Vector3.ProjectOnPlane(m_Camera.transform.forward, Vector3.up).normalized * input.z, Space.World);
+            m_Camera.transform.Translate(Vector3.ProjectOnPlane(m_Camera.transform.right, Vector3.up).normalized * input.x, Space.World);
         }
         // prevent rotation about the forward direction
         m_Camera.transform.rotation = Quaternion.Euler(m_Camera.transform.rotation.eulerAngles.x,
             m_Camera.transform.rotation.eulerAngles.y, 0.0f);
     }
 
-    private Vector3 GetUserInput()
+    private Vector4 GetUserInput()
     {
-        Vector3 input = new Vector3();
+        Vector4 input = new Vector4(0.0f, 0.0f, 0.0f, 0.0f);
         if (Input.GetKey(KeyCode.W))
         {
             input.z = 1.0f;
@@ -77,6 +154,11 @@ public class CameraController : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftShift))
         {
             input.y = -1.0f;
+        }
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            input.w = 1.0f;
         }
 
         return input;
--- Assets/Scripts/Motor.cs
+++ Assets/Scripts/Motor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Motor : MonoBehaviour
 {
     public static readonly float MAX_ANGULAR_SPEED = 10.0f;
@@ -27,11 +28,36 @@ public class Motor : MonoBehaviour
         Rigidbody rb = GetComponent<Rigidbody>();
         //Debug.Log(transform.forward);
         //Debug.Log(rb.angularVelocity);
-        Debug.Log(Vector3.Cross(rb.angularVelocity.normalized, transform.forward.normalized).magnitude);
+        //Debug.Log(Vector3.Cross(rb.angularVelocity.normalized, transform.forward.normalized).magnitude);
         float currentAngularSpeed = Vector3.Project(rb.angularVelocity, transform.forward).magnitude;
-        //Debug.Log(currentAngularSpeed);
-        //rb.AddTorque(new Vector3(0.0f, 0.0f, 100.0f), ForceMode.Force);
-        
+        switch (name)
+        {
+            //Debug.Log(currentAngularSpeed);
+            //rb.AddTorque(new Vector3(0.0f, 0.0f, 100.0f), ForceMode.Force);
+            case "WheelFL":
+            case "WheelRL":
+            {
+                //Debug.Log(transform.localEulerAngles);
+                Vector3 angles = transform.localEulerAngles;
+                angles.x = 90.0f;
+                transform.localEulerAngles = angles;
+                break;
+            }
+            case "WheelFR":
+            case "WheelRR":
+            {
+                //Debug.Log(transform.localEulerAngles);
+                Vector3 angles = transform.localEulerAngles;
+                angles.x = -90.0f;
+                transform.localEulerAngles = angles;
+                break;
+            }
+            default:
+            {
+                Debug.Break();
+                break;
+            }
+        }
     }
 
 }
