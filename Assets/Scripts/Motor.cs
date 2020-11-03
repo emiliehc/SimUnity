125d49c4663bece9217eab74d2ea7c5cfdf45d2b
Hongjun <nanjingchj@gmail.com>
2020-11-03 14:55:59 -0500

    clean up

--- Assets/Scripts/Motor.cs
+++ Assets/Scripts/Motor.cs
@@ -60,38 +60,8 @@ public class Motor : MonoBehaviour
     void Update()
     {
         Rigidbody rb = GetComponent<Rigidbody>();
-        //Debug.Log(transform.forward);
-        //Debug.Log(rb.angularVelocity);
-        //Debug.Log(Vector3.Cross(rb.angularVelocity.normalized, transform.forward.normalized).magnitude);
         float currentAngularSpeed = Vector3.Project(rb.angularVelocity, transform.forward).magnitude;
-        //switch (name)
-        //{
-        //    //Debug.Log(currentAngularSpeed);
-        //    //rb.AddTorque(new Vector3(0.0f, 0.0f, 100.0f), ForceMode.Force);
-        //    case "WheelFL":
-        //    case "WheelRL":
-        //    {
-        //        //Debug.Log(transform.localEulerAngles);
-        //        Vector3 angles = transform.localEulerAngles;
-        //        angles.x = 90.0f;
-        //        transform.localEulerAngles = angles;
-        //        break;
-        //    }
-        //    case "WheelFR":
-        //    case "WheelRR":
-        //    {
-        //        //Debug.Log(transform.localEulerAngles);
-        //        Vector3 angles = transform.localEulerAngles;
-        //        angles.x = -90.0f;
-        //        transform.localEulerAngles = angles;
-        //        break;
-        //    }
-        //    default:
-        //    {
-        //        Debug.Break();
-        //        break;
-        //    }
-        //}
+
         if (TargetAngularSpeed > 0.0f)
         {
             if (currentAngularSpeed < TargetAngularSpeed)
